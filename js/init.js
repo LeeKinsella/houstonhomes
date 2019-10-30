d3cadc260f736485ad7fe4838ac34bfef2e075c1
Lee Kinsella <thelaw@udel.edu>
2019-10-30 00:49:51 -0400

    Added mobile styling

--- js/init.js
+++ js/init.js
@@ -1,8 +1,21 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
+        $('.parallax').parallax();
+        $('.sidenav-trigger').click(function() {
+            $(this).toggleClass('open');
+            if ($(this).hasClass('open')) {
+                $('.sidenav').css("transform", "translateX(0%)");
+            } else {
+                $('.sidenav').css("transform", "translateX(-105%)");
+            }
+        });
+        $('.down-arrow').click(function() {
+            window.scrollTo({
+                top: $('.container > .section').offset().top,
+                left: 0,
+                behavior: 'smooth'
+            });
+        });
 
-//    $('.sidenav').sidenav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+    });
+})(jQuery);
