d855c7c22f3bdb4d35efb9edd26516f639308bd6
Lee Kinsella <thelaw@udel.edu>
2019-12-28 10:16:13 -0500

    Update for GoDaddy hosting

--- js/init.js
+++ js/init.js
@@ -1,21 +1,36 @@
-(function ($) {
-    $(function () {
-        $('.parallax').parallax();
-        $('.sidenav-trigger').click(function() {
-            $(this).toggleClass('open');
-            if ($(this).hasClass('open')) {
-                $('.sidenav').css("transform", "translateX(0%)");
-            } else {
-                $('.sidenav').css("transform", "translateX(-105%)");
-            }
-        });
-        $('.down-arrow').click(function() {
-            window.scrollTo({
-                top: $('.container > .section').offset().top,
-                left: 0,
-                behavior: 'smooth'
-            });
-        });
-
+$(function () {
+    fetch("../header.html")
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        document.querySelector("header").innerHTML = data;
+      });
+    fetch("../footer.html")
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        document.querySelector("footer").innerHTML = data;
+      });
+    $('.sidenav-trigger').click(function() {
+        $(this).toggleClass('open');
+        if ($(this).hasClass('open')) {
+            $('.sidenav').css("transform", "translateX(0%)");
+        } else {
+            $('.sidenav').css("transform", "translateX(-105%)");
+        }
     });
-})(jQuery);
+    $('.down-arrow').click(function() {
+        window.scrollTo({
+            top: $('.container > .section').offset().top,
+            left: 0,
+            behavior: 'smooth'
+        });
+    });
+    window.setTimeout(function() {
+                      
+    $('.parallax').parallax();
+                      });
+
+});
