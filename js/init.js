6a1dd8c3963914251d795a21ae005db43bb2be4f
Lee Kinsella <thelaw@udel.edu>
2020-01-27 17:50:26 -0500

    Second iteration of site layout and styling

--- js/init.js
+++ js/init.js
@@ -1,35 +1,40 @@
 $(function () {
-    fetch("../header.html")
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        document.querySelector("header").innerHTML = data;
-      });
-    fetch("../footer.html")
-      .then(response => {
-        return response.text();
-      })
-      .then(data => {
-        document.querySelector("footer").innerHTML = data;
-      });
-    $('.sidenav-trigger').click(function() {
-        $(this).toggleClass('open');
-        if ($(this).hasClass('open')) {
-            $('.sidenav').css("transform", "translateX(0%)");
-        } else {
-            $('.sidenav').css("transform", "translateX(-105%)");
-        }
-    });
-    $('.down-arrow').click(function() {
-        window.scrollTo({
-            top: $('.container > .section').offset().top,
-            left: 0,
-            behavior: 'smooth'
-        });
-    });
-    window.setTimeout(function() {
-        $('.parallax').parallax();
-    });
+    console.log($('#testimonialsCarousel'));
+    $('#testimonialsCarousel').carousel();
+//    $('.header').height($(window).height());
+//    fetch("../header.html")
+//      .then(response => {
+//        return response.text();
+//      })
+//      .then(data => {
+//        document.querySelector("header").innerHTML = data;
+//      });
+//    fetch("../footer.html")
+//      .then(response => {
+//        return response.text();
+//      })
+//      .then(data => {
+//        document.querySelector("footer").innerHTML = data;
+//      });
+//    $('.sidenav-trigger span').click(function(e) {
+//        console.log(e);
+//        e.preventDefault();
+//        $(this).toggleClass('open');
+//        if ($(this).hasClass('open')) {
+//            $('.sidenav').css("transform", "translateX(0%)");
+//        } else {
+//            $('.sidenav').css("transform", "translateX(-105%)");
+//        }
+//    });
+//    $('.down-arrow').click(function() {
+//        window.scrollTo({
+//            top: $('.container > .section').offset().top,
+//            left: 0,
+//            behavior: 'smooth'
+//        });
+//    });
+//    window.setTimeout(function() {
+//        $('.parallax').parallax();
+//    });
 
 });
